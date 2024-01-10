6bd42498358fe67d7830bda812b9424c297504e2
Parth Kothari <pa20251871@wipro.com>
2024-01-10 11:49:21 +0530

    small changes after initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, ResolveStart, RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
+import { SpinnerService } from './app-modules/core/services/spinner.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'scheduler-ui';
+  constructor(
+    private router: Router,
+    private spinnerService: SpinnerService) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof ResolveStart || event instanceof RouteConfigLoadStart) {
+        this.spinnerService.show();
+      } else if (event instanceof NavigationEnd || event instanceof RouteConfigLoadEnd) {
+        setTimeout(() =>this.spinnerService.hide(), 500);
+      } else if (event instanceof NavigationError) {
+        setTimeout(() =>this.spinnerService.hide(), 500);
+      } else if (() =>event instanceof NavigationCancel) {
+        setTimeout(() =>this.spinnerService.hide(), 500);
+      }
+    });
+  }
 }
